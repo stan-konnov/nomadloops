3996c9f666bd4d8cda3c0f53123cba09d71e3524
Stan Konnov <stanis.konnov@gmail.com>
2025-07-06 17:41:07 +0200

    feature: Expand category enum with more categories.

--- backend/app/utils/enums.py
+++ backend/app/utils/enums.py
@@ -4,8 +4,29 @@ from enum import Enum
 class PlaceCategory(str, Enum):
     """Enum of possible place categories in a nomad loop."""
 
+    # Apartments, hostels, Airbnbs
     LIVING = "living"
+
+    # Coworking spaces and cafes with Wi-Fi
     WORKING = "working"
+
+    # Laundromats
+    LAUNDRY = "laundry"
+
+    # Gyms
     TRAINING = "training"
-    SHOPPING = "shopping"
-    ACTIVITIES = "activities"
+
+    # Metro, bus stations, scooter rentals
+    TRANSPORT = "transport"
+
+    # ATMs, bank branches, currency exchange
+    BANKING_FINANCE = "banking_finance"
+
+    # Grocery stores and supermarkets
+    GROCERY_SHOPPING = "grocery_shopping"
+
+    # Clinics, pharmacies, spas
+    HEALTHCARE_WELLNESS = "healthcare_wellness"
+
+    # Police, fire stations, consulates
+    EMERGENCY_ESSENTIALS = "emergency_essentials"
