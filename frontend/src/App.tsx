50bd56fe2dac30ed9e2ce84dea3648bc2cbdf76a
Stan Konnov <stanis.konnov@gmail.com>
2025-07-09 22:30:40 +0200

    chore: Plug in the styles, I missed that.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 
+import '@src/styles/index.css';
+
 const App = (): ReactNode => {
   return <div className="text-center text-2xl text-blue-600 mt-20">🚀 NomadLoops Frontend</div>;
 };
