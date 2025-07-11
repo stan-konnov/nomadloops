390703782665dc78983868da735445ccef7e59df
Stan Konnov <stanis.konnov@gmail.com>
2025-07-11 16:00:04 +0200

    bugfix: Rename environment variables.

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
   headers: { 'Content-Type': 'application/json' },
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/${import.meta.env.VITE_API_VERS}`,
+  baseURL: `${import.meta.env.VITE_API_URL}/${import.meta.env.VITE_API_VER}`,
 });
