8cdbfe57a8f65af890f481fb43b2b5b867be4899
Stan Konnov <stanis.konnov@gmail.com>
2025-07-11 22:34:05 +0200

    chore: Simplify state.

--- frontend/src/store/loops.planner.store.ts
+++ frontend/src/store/loops.planner.store.ts
@@ -4,14 +4,10 @@ import { create } from 'zustand';
 
 type LoopsPlannerState = {
   city: string;
-  coordinates: { lat: number; lng: number } | null;
-
   setCity: (name: string) => void;
 };
 
 export const useLoopsPlannerStore = create<LoopsPlannerState>((set) => ({
   city: '',
-  coordinates: null,
-
   setCity: (city): void => set({ city }),
 }));
