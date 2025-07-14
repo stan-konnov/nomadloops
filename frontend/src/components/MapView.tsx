b0f635f9169f45e983bd4f100c3c90ab561978fc
Stan Konnov <stanis.konnov@gmail.com>
2025-07-14 15:52:16 +0200

    Feature: WIP on centering and zooming the map on user input.

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -1,16 +1,69 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useRef } from 'react';
+import { useLoopsPlannerStore } from '@src/store/loops.planner.store';
+
+const MapCenterUpdater = ({
+  coordinates,
+  zoom,
+}: {
+  coordinates: LatLngExpression;
+  zoom: number;
+}): null => {
+  const map = useMap();
+  const hasZoomed = useRef(false);
+
+  useEffect(() => {
+    if (!coordinates) {
+      return;
+    }
+
+    const onUserZoom = (): void => {
+      hasZoomed.current = true;
+    };
+    map.on('zoomend', onUserZoom);
+
+    if (!hasZoomed.current) {
+      // Animate to coordinates and zoom on first render
+      map.flyTo(coordinates, zoom);
+    } else {
+      // Animate to coordinates but keep current zoom on user zoom
+      map.flyTo(coordinates, map.getZoom());
+    }
+
+    return (): void => {
+      map.off('zoomend', onUserZoom);
+    };
+  }, [coordinates, zoom, map]);
+
+  return null;
+};
 
 export const MapView = (): ReactElement => {
+  const { cityCoordinates } = useLoopsPlannerStore();
+
+  // Default to Kuala Lumpur, Malaysia
   const defaultCenter: LatLngExpression = [3.139, 101.6869];
+  const center: LatLngExpression =
+    cityCoordinates?.lat && cityCoordinates?.lng
+      ? [cityCoordinates.lat, cityCoordinates.lng]
+      : defaultCenter;
+
+  const defaultZoom = 5;
+  const zoomOnInput = cityCoordinates?.lat && cityCoordinates?.lng ? 10 : defaultZoom;
 
   return (
-    <MapContainer className="w-full h-full" center={defaultCenter} zoom={5} scrollWheelZoom={true}>
+    <MapContainer
+      className="w-full h-full"
+      center={defaultCenter}
+      zoom={defaultZoom}
+      scrollWheelZoom={true}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {cityCoordinates && <MapCenterUpdater coordinates={center} zoom={zoomOnInput} />}
     </MapContainer>
   );
 };
