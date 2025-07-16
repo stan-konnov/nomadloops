f9da1a9f6c7a436fd0438c3193073b731d6aebf8
Stan Konnov <stanis.konnov@gmail.com>
2025-07-16 10:30:54 +0200

    chore: Setup for client side testing.

---
+++ frontend/vitest.config.js
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/setup.ts',
+    coverage: {
+      reporter: ['text', 'html'],
+    },
+  },
+});
