f396f4c7156c3abd2f93367bd16f2986b911940b
Stan Konnov <stanis.konnov@gmail.com>
2025-07-16 11:03:04 +0200

    chore: Reworking some setups.

--- frontend/src/components/LoopsPlanner.test.tsx
+++ frontend/src/components/LoopsPlanner.test.tsx
@@ -3,8 +3,11 @@ import { render, screen } from '@testing-library/react';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 
 import { LoopsPlanner } from '@src/components/LoopsPlanner';
-import * as store from '@src/store/loops.planner.store';
 import { LoopsGenerationStatus, PlaceCategory } from '@src/utils/enums';
+
+import * as store from '@src/store/loops.planner.store';
+import * as api from '@src/api/loops.api';
+import * as geocode from '@src/utils/geocode';
 
 vi.mock('./MapView', () => ({
   MapView: (): ReactElement => <div data-testid="map-view">Mock MapView</div>,
@@ -14,27 +17,29 @@ vi.mock('./LoopsForm', () => ({
   LoopsForm: (): ReactElement => <div data-testid="loops-form">Mock LoopsForm</div>,
 }));
 
+const mockStore = {
+  city: 'Test City',
+  setCity: vi.fn(),
+
+  monthlyBudget: 1000,
+  setMonthlyBudget: vi.fn(),
+
+  selectedCategories: new Set<PlaceCategory>(),
+  setSelectedCategories: vi.fn(),
+
+  numberOfLoopsToGenerate: 1,
+  setCityCoordinates: vi.fn(),
+
+  loopsGenerationStatus: LoopsGenerationStatus.NOT_STARTED,
+  setLoopsGenerationStatus: vi.fn(),
+
+  generatedLoops: [],
+  setGeneratedLoops: vi.fn(),
+};
+
 describe('<LoopsPlanner />', () => {
   beforeEach(() => {
-    vi.spyOn(store, 'useLoopsPlannerStore').mockReturnValue({
-      city: 'Test City',
-      setCity: vi.fn(),
-
-      monthlyBudget: 1000,
-      setMonthlyBudget: vi.fn(),
-
-      selectedCategories: new Set<PlaceCategory>(),
-      setSelectedCategories: vi.fn(),
-
-      numberOfLoopsToGenerate: 1,
-      setCityCoordinates: vi.fn(),
-
-      loopsGenerationStatus: LoopsGenerationStatus.NOT_STARTED,
-      setLoopsGenerationStatus: vi.fn(),
-
-      generatedLoops: [],
-      setGeneratedLoops: vi.fn(),
-    });
+    vi.spyOn(store, 'useLoopsPlannerStore').mockReturnValue(mockStore);
   });
 
   it('renders MapView and LoopsForm', () => {
@@ -42,5 +47,53 @@ describe('<LoopsPlanner />', () => {
 
     expect(screen.getByTestId('map-view')).toBeInTheDocument();
     expect(screen.getByTestId('loops-form')).toBeInTheDocument();
+  });
+});
+
+describe('<LoopsPlanner /> side-effects', () => {
+  // const mockSetCityCoordinates = vi.fn();
+  // const mockSetLoopsGenerationStatus = vi.fn();
+  // const mockSetGeneratedLoops = vi.fn();
+
+  beforeEach(() => {
+    vi.resetAllMocks();
+
+    vi.spyOn(store, 'useLoopsPlannerStore').mockReturnValue(mockStore);
+
+    vi.spyOn(geocode, 'geocodeCity').mockResolvedValue({ lat: 1, lng: 2 });
+
+    vi.spyOn(api, 'createLoopsRequest').mockResolvedValue({
+      success: true,
+      message: 'Loops generation started successfully.',
+    });
+
+    vi.spyOn(api, 'getLoopsStatusRequest').mockResolvedValue({
+      success: true,
+      message: 'Loops generation status retrieved successfully.',
+      data: LoopsGenerationStatus.GENERATING,
+    });
+
+    vi.spyOn(api, 'getLoopsRequest').mockResolvedValue({
+      success: true,
+      message: 'Generated loops retrieved successfully.',
+      data: [
+        {
+          city: 'Kuala Lumpur',
+          places: [
+            {
+              name: 'Nomad Nest Hostel',
+              category: PlaceCategory.LIVING,
+              address: '123 Sukhumvit Soi 11, Bangkok',
+              url: 'https://maps.google.com/?q=Nomad+Nest+Hostel',
+              coordinates: {
+                lat: 13.743,
+                lng: 100.535,
+              },
+              price: 350,
+            },
+          ],
+        },
+      ],
+    });
   });
 });
